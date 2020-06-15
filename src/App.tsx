ee8a8eb184514de495612d92a21bfea8a5f4b63a
Yapichoy <torezracer@gmail.com>
2020-06-15 17:02:16 +0300

    second section compleate

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './index.scss';
 import Header from "./components/Header/Header";
 import TestAssigmentScreen from "./components/TestAssigmentScreen/TestAssigmentScreen";
+import SecondSection from "./components/SecondSection/SecondSection";
 function App() {
   return (
     <div className="App">
       <Header/>
       <TestAssigmentScreen/>
+      <SecondSection/>
     </div>
   );
 }
---
+++ src/components/SecondSection/SecondSection.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import manLaptop from "../../assets/man-laptop-v1.svg";
+import {NavLink} from "react-router-dom";
+const SecondSection = () => {
+    return (
+        <div className={"container section second_section"}>
+            <div className="row">
+                <h1>Let's get acquainted</h1>
+            </div>
+            <div className="row bottom-row">
+                <div className="photo-wrapper">
+                    <img src={manLaptop} alt=""/>
+                </div>
+                <div className="description-wrapper">
+                    <h3>I am cool frontend developer</h3>
+                    <p>We will evaluate how clean your approach to writing CSS and Javascript code is. You can use any CSS and Javascript 3rd party libraries without any restriction.</p>
+                    <p className={"last-text"}>If 3rd party css/javascript libraries are added to the project via bower/npm/yarn you will get bonus points. If you use any task runner (gulp/webpack) you will get bonus points as well. Slice service directory page P​SD mockup​ into HTML5/CSS3. </p>
+                    <NavLink className={'link'} to={"/"}>Sing up now</NavLink>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SecondSection;
